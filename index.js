2dc34488ccff090099b8fd7cf6334993a27cb661
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-11-22 15:52:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//const http = require("http");
+
+//const hostname = "localhost";
+
+//const server = http.createServer((request, response) => {
+//    response.statusCode = 200;
+//    response.setHeader("Content-Type", "text/plain");
+//    response.end("Hello World!");
+//})
+
+//server.listen(3333, hostname, () => {
+//    console.log("Server running in localhost:3333");
+//});
+
+import express from "express";
+
