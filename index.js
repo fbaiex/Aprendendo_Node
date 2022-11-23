d10ed145fa2acd97a03d986f72bde5ee94beab00
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-11-23 10:29:05 -0300

    Aprendendo Nodejs

--- index.js
+++ index.js
@@ -13,4 +13,22 @@
 //});
 
 import express from "express";
+import fs from "fs";
+import https from "https";
+import cors from "cors";
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) =>{
+    res.json({ message: "hello world"});
+});
+
+app.listen(3000, () => console.log("api rodando"));
+
+https.createServer({
+    cert: fs.readFileSync("C:/Users/fabio/OneDrive/Área de Trabalho/StackX/web3.0/Node/certificate.crt"),
+    key: fs.readFileSync("C:/Users/fabio/OneDrive/Área de Trabalho/StackX/web3.0/Node/key.key")
+}, app)
+.listen(3001, () => console.log("Rodando em https"));
